761ef67820fc5880c9857f7dc77e9f3d66208a56
Deviant <112615715+DEViantUA@users.noreply.github.com>
2022-09-23 18:54:19 +0300

    Add files via upload

---
+++ GenshinWiki/wiki_api_params.py
@@ -0,0 +1,18 @@
+# Copyright 2022 DEViantUa <t.me/deviant_ua>
+# All rights reserved.
+__all__ = ["url",
+    "paramsDb",
+    "urlAmbr"
+]
+
+
+url = "https://genshin-db-api.vercel.app/api/"
+
+paramsDb = {"query": None,
+    "matchCategories": "true",
+    "dumpResult": "true",
+    "resultLanguage": "Russian",
+    "queryLanguages": "Russian,jap"
+}
+
+urlAmbr = "https://api.ambr.top/v2/"
