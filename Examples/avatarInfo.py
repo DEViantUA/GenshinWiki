56f6c9e57e2604d692f4a41f2dc6b5aa7495edef
Deviant <112615715+DEViantUA@users.noreply.github.com>
2022-09-24 10:52:43 +0300

    Update avatarInfo.py

--- Examples/avatarInfo.py
+++ Examples/avatarInfo.py
@@ -3,9 +3,10 @@ from GenshinWiki.wiki import WikiAmbr, WikiDb
 ambr = WikiAmbr(lang = "en")
 
 avatarInfo = ambr.avatar()
+db = WikiDb(lang = "en")
 
 for avatarNames in avatarInfo:
-    info = WikiDb(lang = "en").avatar(avatarName = avatarInfo[avatarNames]["name"])
+    info = db.avatar(avatarName = avatarInfo[avatarNames]["name"])
     print(f'\n==={info["name"]}===')
     print(f'FullName: {info["fullname"]}')
     print(f'Description: {info["description"]}')
